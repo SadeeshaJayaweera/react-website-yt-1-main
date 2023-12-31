88b58fea6146c4161466a72f3bf2aee06364217c
SadeeshaJayaweera <sadeesha.jayaweera@gmail.com>
2023-12-31 17:18:13 +0530

    Updated Version

--- src/components/After_Login/BoardingFilter.js
+++ src/components/After_Login/BoardingFilter.js
@@ -5,12 +5,15 @@ const BoardingFilter = ({ onFilterChange }) => {
     const [priceFilter, setPriceFilter] = useState('0');
     const [ratingFilter, setRatingFilter] = useState('1');
     const [distanceFilter, setDistanceFilter] = useState('0');
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     const handleFilterChange = () => {
         // Validate input values
         const validatedPriceFilter = Math.max(0, parseFloat(priceFilter)) || ''; // Ensure price is non-negative
-        const validatedRatingFilter = Math.max(1, Math.min(5, parseFloat(ratingFilter))) || ''; // Ensure rating is between 1 and 5
-        const validatedDistanceFilter = Math.max(0, parseFloat(distanceFilter)) || ''; // Ensure distance is non-negative
+        const validatedRatingFilter =
+            Math.max(1, Math.min(5, parseFloat(ratingFilter))) || ''; // Ensure rating is between 1 and 5
+        const validatedDistanceFilter =
+            Math.max(0, parseFloat(distanceFilter)) || ''; // Ensure distance is non-negative
 
         // Call the onFilterChange prop with the validated filter values
         onFilterChange({
@@ -21,13 +24,18 @@ const BoardingFilter = ({ onFilterChange }) => {
         });
     };
 
+    // Toggle dark mode
+    const toggleDarkMode = () => {
+        setIsDarkMode((prevMode) => !prevMode);
+    };
+
     // Add an event listener to call handleFilterChange when the button is clicked
     const applyFilters = () => {
         handleFilterChange();
     };
 
     return (
-        <div className="boarding-filter">
+        <div className={`boarding-filter${isDarkMode ? ' dark-mode' : ''}`}>
             <h3>Boarding Filters</h3>
             <input
                 type="text"
@@ -63,6 +71,10 @@ const BoardingFilter = ({ onFilterChange }) => {
             </label>
             {/* Use the applyFilters function as the onClick handler for the button */}
             <button onClick={applyFilters}>Apply Filters</button>
+            {/* Toggle dark mode button */}
+            <button id="toggle-dark-mode" onClick={toggleDarkMode}>
+                Toggle Dark Mode
+            </button>
         </div>
     );
 };
